3e9651e93c12166e71e6aae8e61613daecb90d5e
Neto Trindade <neto.trindade@live.com>
2025-09-27 21:17:55 -0300

    produtos renderizados e categorizados
    produtos renderizados e categorizados

---
+++ src/components/CardProduto.jsx
@@ -0,0 +1,19 @@
+import PlantImage from '../assets/Plant.png'
+import CowImage from '../assets/Cow.png'
+
+export default function CardProduto ({ item }) {
+    return (
+        <a href={`/product/${item.id}`} className="products__list--item">
+            <img src={item.imagem} alt="" />
+            <h3 className="products__list--price">{item.preco.por} <span>{item.preco.de}</span></h3>
+            <h4 className="products__list--name">{item.nome}</h4>
+            <div className="product__tag">
+                {
+                    item.vegano ? 
+                    <><img src={PlantImage} alt="planta" /> <span>Vegano</span></> :
+                    <><img src={CowImage} alt="vaca" /> <span>Contém Lactose</span></>
+                }                
+            </div>
+        </a>
+    )
+}
